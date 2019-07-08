e0c26edff1778db1fc4e8604cfe54f53d8ee9381
SophYo <sophiya.dimitrova@gmail.com>
2019-07-08 20:44:07 +0300

    implementing try catch exceptions to class Main

--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,15 @@ public class Main {
                 );
 
         String inputPeople = reader.readLine();
-        String[] tokens = inputPeople.split(";");
-
-        Map<String, Double> peopleAndMoney = new LinkedHashMap<>();
+        String[] tokensPeople = inputPeople.split(";");
         List<Person> people = new ArrayList<>();
 
-        for (String token : tokens) {
+        for (String token : tokensPeople) {
             String[] data = token.split("=");
+
             try {
                 Person person = new Person(data[0], Double.parseDouble(data[1]));
+                people.add(person);
 
             } catch (IllegalArgumentException message) {
                 System.out.println(message.getMessage());
@@ -29,5 +29,19 @@ public class Main {
 
         }
 
+        String inputProducts = reader.readLine();
+        String[] tokensProducts = inputProducts.split(";");
+        List<Product> products = new ArrayList<>();
+
+        for (String product : tokensProducts) {
+            String[] data = product.split("=");
+
+            try {
+                Product product1 = new Product(data[0], Double.parseDouble(data[1]));
+                products.add(product1);
+            } catch (IllegalArgumentException message) {
+                System.out.println(message.getMessage());
+            }
+        }
     }
 }
