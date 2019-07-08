8e7834b1fcb8934bd93248bef8d57bb786f2c06a
SophYo <sophiya.dimitrova@gmail.com>
2019-07-08 20:38:20 +0300

    implementing try catch exceptions to class Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -18,12 +16,18 @@ public class Main {
         String[] tokens = inputPeople.split(";");
 
         Map<String, Double> peopleAndMoney = new LinkedHashMap<>();
+        List<Person> people = new ArrayList<>();
 
         for (String token : tokens) {
             String[] data = token.split("=");
-            peopleAndMoney.put(data[0], Double.parseDouble(data[1]));
+            try {
+                Person person = new Person(data[0], Double.parseDouble(data[1]));
+
+            } catch (IllegalArgumentException message) {
+                System.out.println(message.getMessage());
+            }
+
         }
 
-        System.out.println(Arrays.toString(tokens));
     }
 }
