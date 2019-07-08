067a037e7484350a1ce89bbf6e3cdd3173de4ecd
SophYo <sophiya.dimitrova@gmail.com>
2019-07-08 16:39:01 +0300

    validationMoney() added

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Person {
@@ -5,6 +6,26 @@ public class Person {
     private  double money;
     private List<Product> products;
 
+    public Person(String name, double money) {
+        this.setName(name);
+        this.setMoney(money);
+        this.products = new ArrayList<>();
+    }
+
+    private void setMoney(double money) {
+        if (money < 0) {
+            validationMoney();
+        }
+        this.money = money;
+    }
+
+    private void setName(String name) {
+
+    }
+
+    private void validationMoney() {
+        throw new IllegalArgumentException("Money cannot be negative");
+    }
 
 
 }
